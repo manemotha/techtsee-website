e0803c1600fd47075b5b790aa04158f34fc8927d
manemotha <mane.motha@proton.me>
2025-11-02 20:08:53 +0200

    add a Shop page and styles, move shop styles to index.css for multiple usage from index.twig & shop.twig

--- src/routes.php
+++ src/routes.php
@@ -86,9 +86,54 @@ return function(App $app) {
         return $view->render($response, 'index.twig', [
             'title'=>'Home',
             'isAuthorized'=>false,
+            'URL_BASENAME'=> '/techtsee', // notice...
             'services'=>$services,
             'products'=>$products,
             'request'=>$request
+        ]);
+    });
+
+    $app->get('/shop', function(Request $request, Response $response) {
+
+        $products = [
+            [
+                'name'=>'CCTV Camera',
+                'description'=>'Home security cameras video surveillance systems isolated vector illustration. Pro Vector',
+                'tags'=>['Security', 'Camera', 'CCTV'],
+                'image_url'=>'https://dubaimachines.com/pub/media/wysiwyg/Ip_Cameras.jpg',
+                'image_alt'=>'CCTV Camera',
+                'price'=>699.99
+            ], [
+                'name'=>'HP LaserJet',
+                'description'=>'HP NeverStop LASERJET 1200W All in one Printer. Print/Scan/Cop',
+                'tags'=>['Laserjet', 'Scan', 'Print'],
+                'image_url'=>'https://i5.walmartimages.com/seo/HP-Color-LaserJet-Pro-MFP-3301sdw-Wireless-Laser-Color-Printer_6e94955a-835a-4c2d-91a9-6f4c678e265e.57e4d6cbb17d266058e1bd984cd2c6fe.jpeg',
+                'image_alt'=>'HP Laserjet Printer',
+                'price'=>4600.00
+            ], [
+                'name'=>'IGUGNIK Toner Cartridge',
+                'description'=>'4 Pack 218A BK/C/M/Y Toner Cartridge (with Chip) | Replacement for HP 218A Work with 3201dw MFP 3301cdw MFP 3301fdw MFP 3301sdw | W2180A W2181A W2182A W2183A',
+                'tags'=>['Toner', 'Printer', 'Cartridge'],
+                'image_url'=>'https://m.media-amazon.com/images/I/712GnELgnIL._AC_.jpg',
+                'image_alt'=>'Toner Cartridge',
+                'price'=>749.99
+            ], [
+                'name'=>'Apple 15-inch MacBook Air M4',
+                'description'=>'2025 Apple MacBook Air 15 3 M4 Processor 16GB RAM 256GB SSD',
+                'tags'=>['Apple', 'Laptop', 'Macbook'],
+                'image_url'=>'https://media.product.which.co.uk/prod/images/original/21246656a3ee-apple-macbook-pro-16-inch-2001.jpg',
+                'image_alt'=>'Apple Macbook',
+                'price'=>8999.99
+            ]
+        ];
+
+        $view = Twig::fromRequest($request);
+        return $view->render($response, 'shop.twig', [
+            'title'=>'Shop',
+            'URL_BASENAME'=> '/techtsee', // notice...
+            'isAuthorized'=>false,
+            'request'=>$request,
+            'products'=>$products
         ]);
     });
 
